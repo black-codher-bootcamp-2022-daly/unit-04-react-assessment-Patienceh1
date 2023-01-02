b6d0a3c74f11872cd513880dce6d158c7d0a0972
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-02 15:09:32 +0000

    button-fix

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,9 +1,24 @@
 import React from "react";
+import Product from "./Product";
 
 
-export default function () {
+
+export default function Basket ({basket}) {
+ console.log(basket)
 
     return (
-        <h1>Basket</h1>
-    )
+        <div>
+        {basket ? (
+          <div>
+            <h1>Basket</h1>
+            <h1>{basket.length}</h1>
+            {basket.map((product) => (
+              <Product product={product} key={product.trackId} isInTheBasket={true} />
+            ))}
+          </div>
+        ) : (
+          <h1>No products yet</h1>
+        )}
+      </div>
+    );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,23 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import App from "../App";
 
-const Product = (props) => {
-  console.log(props);
+const Product = ({product,...props}) => {
+  console.log(props)
 
-  const { product } = props;
+  const {  trackName, trackId, artistName, trackPrice, currency, artworkUrl100 } = product ||{} ;
   return (
-    <div id="product-container">
-        <img src={props.product.artworkUrl100}
-        alt={props.product.artistName} />
+    <div id={"product-container"}>
+        <img src={artworkUrl100}
+        alt={artistName} />
       <ul className="list">
-        <h2>{props.product.trackName}</h2>
-        <h2>{props.product.artistName}</h2>
+        <h2>{trackName}</h2>
+        <h2>{artistName}</h2>
         <h4>
-          {props.product.trackPrice}
-          {props.product.currency}
+          {trackPrice}
+          {currency}
         </h4>
       </ul>
-      <button className="add-button" onClick={() => props.addToBasket(props.product.trackId)}> { props.isInTheBasket ? "Remove" : "Add to basket"}</button>
+      <button  id="add-button" onClick={() => props.addToBasket(trackId)}> { props.isInTheBasket ? "Remove" : "Add to basket"}</button>
     </div>
   );
 };
