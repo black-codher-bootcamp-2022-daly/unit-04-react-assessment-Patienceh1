35c844a61855fc1e085ab15baded583c0a49e1ca
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-06 10:57:24 +0000

    classes

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -10,21 +10,21 @@ export default function Basket ({removeFromBasket, basket, basketTotal}) {
 const count = basket.length;
     return (
         <div>
-        {basket.length > 0 ? (
-          <div>
+       
+         
             <h1>Basket</h1>
             <BasketCount BasketCount={count}/>
-            
-            {/* <h1>{basket.length}</h1> */}
-            {basket.map((product) => (
+
+            {basket.length > 0 ? (
+            basket.map((product) => (
                 <div className="product" key={product.trackId}>
               <Product item={product} key={product.trackId} inBasket={product.inBasket} removeFromBasket={removeFromBasket} />
               </div>
-            ))}
-          </div>
+            ))
+         
         ) : (
-          <div className="empty">Sorry no items in basket...</div>
-        )}
+          <div className="empty">Sorry, no items in basket...</div>
+        ) }
         <BasketTotal basketTotal={basketTotal} />
       </div>
     );
