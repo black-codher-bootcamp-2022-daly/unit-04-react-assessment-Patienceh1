4b5bd2390f43efb6d3f9efd4e1d825f0d4c8ce18
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-06 13:34:24 +0000

    basket-fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles/App.css";
 import Header from "./components/Header";
-import { Routes, Route,  BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import { useState } from "react";
 import data from "./models/data.json";
 import About from "./pages/About";
@@ -23,20 +23,36 @@ function App() {
         product.inBasket = true;
         console.log(product);
         setBasket((prev) => [...prev, product]);
-        setTotal(total + product.trackPrice);
-        setCount(count +1)
+
+        if (product.trackPrice) {
+          setTotal(parseFloat(( total + product.trackPrice)));
+        } else {
+          setTotal(total + 0);
+        }
       }
+      console.log(setTotal);
+      setCount(count + 1);
     });
   }
 
   function removeFromBasket(trackId) {
-    const removeFromCart = removeProduct.filter(
-      (products) => products.trackId !== trackId
+    const removeFromCart= [];
+      basket.filter((products) => 
+      { if (products.trackId !== trackId) {
+        removeFromCart.push(products);
+      } else {
+        products.trackId = !products.trackId
+      if (products.trackPrice) {
+        setTotal(parseFloat((total - products.trackPrice)));
+      } }
+      }
+     
     );
-    basket.shift(trackId);
-    setRemoveProduct(removeFromCart);
-    setTotal(total - products.trackPrice);
-    setCount(count -1)
+  
+    setBasket(removeFromCart);
+  
+    
+    setCount(count - 1);
     // console.log(removeFromCart,basket)
     // products.map((product) => {
     //   if (product.trackId === trackId) {
@@ -91,14 +107,14 @@ function App() {
   }
   return (
     <Router>
-    <div className="App">
-      <Header basketCount={count}/>
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/basket" element={<BasketList />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Header basketCount={count} />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/basket" element={<BasketList />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
