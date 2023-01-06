607d65b35dd291f253ceab0ee996af5ab6f0438d
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-06 12:14:10 +0000

    header-test

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 export default function Header (props) {
 return <div id="navigation"><h1 className="title">Media store</h1>
 <div>
-    <Link to="/" id="homelink"> <h4>Home</h4></Link>
+    <Link  className="Link" to="/" id="homelink"> <h4>Home</h4></Link>
 </div>
 <div>
-    <Link to="/about" id="aboutlink"><h4>About </h4></Link> 
+    <Link className="Link" to="/about" id="aboutlink"><h4>About </h4></Link> 
 </div>
 <div>
-    <Link to="/basket" id="basketlink"><h4>Basket:{props.basketCount}</h4></Link> 
+    <Link className="Link" to="/basket" id="basketlink"><h4>Basket:{props.basketCount}</h4></Link> 
 </div>
 </div>
 }
