468a30ab03027d52e495a247f82bab9d3db05d1b
Patience Hodzi <patiencehodzi@gmail.com>
2023-01-15 22:14:04 +0000

    about-page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,7 @@ export default function About () {
     return (
         <>
         <h1>About</h1>
+        <p>Browse films, music, and tv shows in this one stop App. See something you like? simply add the Item to your basket. One finished, find your items in your cart where you can track the total amount of all the items in your basket.</p>
         <p>The maker of this App is Patience Hodzi. Patience is currenctly enrolled at BlackCodHer FullStack Development Bootcamp.
         </p>
         </>
